1c36b7ae9c2b1f888952d88cac0cd025f6bc315f
jayson092301 <jaysondaniel805@gmail.com>
2024-11-27 19:13:18 +0800

    commit

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
     <View>
-      <Signin/>
+      <Login/>
       <StatusBar style="auto" />
     </View>
     </SafeAreaView>
